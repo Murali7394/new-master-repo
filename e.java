ceedbd242eda84a235b74b9e2f14a090d33852b9
Murali7394 <110376172+Murali7394@users.noreply.github.com>
2022-09-01 18:11:22 +0530

    Create e.java

---
+++ e.java
@@ -0,0 +1,20 @@
+public calss{
+  
+  
+  
+public static void main(String[] args){
+  String s = "madam";
+  String r = "";
+  
+  for(int i=s.length()-1;i>=0;i--){
+  
+  r=r+s.charAt(i);
+  
+  
+  }
+  System.out.println("the reverse of the string is  :"+r)
+
+
+}
+
+}
